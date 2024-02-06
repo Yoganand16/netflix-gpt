afd189651771065967f8f75550edcc9c8def97a6
Yoganand16 <yoganandh.129@gmail.com>
2024-02-06 15:36:26 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className=" text-3xl font-bold text-red-500">
+       Namaste Everyone lets build netflix gpt 
+
+    </div>
+  );
+}
+
+export default App;
